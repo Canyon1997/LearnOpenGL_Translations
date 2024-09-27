540d677c346ba9610e0364ecc8a0983a447e3f10
Canyon Davidson <canyon.davidson@gmail.com>
2024-09-26 23:03:05 -0400

    added folders for shaders and textures that will be needed for tomorrow. Trying to challenge myself by rendering the results from Textures befure using that object/shaders to rotate it in the next learning module

--- OpenGL_Transformations/main.cpp
+++ OpenGL_Transformations/main.cpp
@@ -16,16 +16,45 @@
 
 int main()
 {
-	//GLFWwindow* window = InitializeOpenGL("Translations");
+	GLFWwindow* window = InitializeOpenGL("Translations");
 
+	Shader woodenShader("Shaders//WoodenVertexShader.glsl", "Shaders//WoodenFragmentShader.glsl");
+
+	float vertices[]{
+		0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f, // bottom left
+		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right
+		0.0f, 0.5f, 0.0f,   0.0f, 0.0f, 1.0f // top middle
+	};
+
+	/*
 	// Translates a vector by "trans" identity value
 	glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
 	glm::mat4 trans = glm::mat4(1.0f);
 	trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
 	vec = trans * vec;
 	std::cout << vec.x << vec.y << vec.z << std::endl;
+
+	// Scale and rotate object
+	glm::mat4 trans2 = glm::mat4(1.0f);
+	trans2 = glm::rotate(trans2, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
+	trans2 = glm::scale(trans2, glm::vec3(0.5, 0.5, 0.5));
+	*/
+
+	unsigned int VBO, VAO;
+
+	// VBO
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(VBO, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// VAO
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+
 	
-	/*
+	
+	
 	while (!glfwWindowShouldClose(window))
 	{
 		ProcessInput(window);
@@ -43,7 +72,7 @@ int main()
 
 
 	// Clean-up
-	glfwTerminate();*/
+	glfwTerminate();
 
 	return 0;
 }
