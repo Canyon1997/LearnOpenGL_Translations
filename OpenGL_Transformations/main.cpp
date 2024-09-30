e17e52e593efdde908d6961f2ac149bd5dfe440f
Canyon Davidson <canyon.davidson@gmail.com>
2024-09-29 23:54:42 -0400

    recap of adding wooden texture onto square, doing recap of adding second texture tomorrow

--- OpenGL_Transformations/main.cpp
+++ OpenGL_Transformations/main.cpp
@@ -20,10 +20,10 @@ int main()
 	Shader woodenShader("Shaders//WoodenVertexShader.glsl", "Shaders//WoodenFragmentShader.glsl");
 
 	float vertices[]{
-		0.5f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f, // bottom right
-		0.5f, 0.5f, 0.0f,     0.0f, 1.0f, 0.0f, // top right
-		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f, // bottom left
-		-0.5f, 0.5f, 0.0f,    1.0f, 1.0f, 0.0f, // top left
+		0.5f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f,   1.0f, 0.0f, // bottom right
+		0.5f, 0.5f, 0.0f,     0.0f, 1.0f, 0.0f,   1.0f, 1.0f,// top right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,// bottom left
+		-0.5f, 0.5f, 0.0f,    1.0f, 1.0f, 0.0f,   0.0f, 1.0f// top left
 	};
 
 	unsigned int indices[]{
@@ -31,27 +31,52 @@ int main()
 		2, 3, 1
 	};
 
-	unsigned int VBO, VAO, EBO;
+	unsigned int VBO, VAO, EBO, TBO;
 
-	// VBO
+	// Vertex Buffer Object
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// EBO
+	// Element Buffer Object
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	// VAO
+	// Texture Buffer Object
+	glGenBuffers(1, &TBO);
+	glBindBuffer(GL_TEXTURE_2D, TBO);
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int width, height, woodNRChannel;
+	unsigned char* woodTextureData = stbi_load("Textures//woodencontainer.jpg", &width, &height, &woodNRChannel, 0);
+	if (woodTextureData != nullptr)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, woodTextureData);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+		return -1;
+	}
+	stbi_image_free(woodTextureData);
+
+
+	// Vertex Array Object
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-	woodenShader.Use();
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 	
 	while (!glfwWindowShouldClose(window))
 	{
@@ -61,6 +86,8 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glBindTexture(GL_TEXTURE_2D, TBO);
+		woodenShader.Use();
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, indices);
 		
